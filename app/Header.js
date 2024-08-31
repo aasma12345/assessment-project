45addb4d9f25f52086a1023faeeacd01074905c3
aasma <ubuntu-clone@example.com>
2024-08-31 20:43:07 +0530

    complete

--- app/Header.js
+++ app/Header.js
@@ -2,14 +2,14 @@ import React from 'react';
 function Header() {
     return (
         <>
-            <div className='des-only'>
-                <div className="header-row">
-                    <div className="header">
-                        <img className="header-logo" src="Logo Grp.png" alt="Logo" />
-                        <p className="header-text">QA</p>
-                    </div>
+
+            <div className="header-row">
+                <div className="header">
+                    <img className="header-logo" src="Logo Grp.png" alt="Logo" />
+                    <p className="header-text">QA</p>
                 </div>
             </div>
+
 
 
         </>
