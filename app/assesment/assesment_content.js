6413365e28d88b2024d968050981a5597be1f90b
aasma <ubuntu-clone@example.com>
2024-09-03 14:13:38 +0530

    ok

--- app/assesment/assesment_content.js
+++ app/assesment/assesment_content.js
@@ -1,20 +1,24 @@
 import Filter from "./filter";
 import Container from "./container";
 import Timers from './timer';
+import Footer from './Footer';
 const Content = () => {
     return (
-        <div className="content-box">
-            <Filter />
-            <Timers />
-            <div className="questions">
-                <Container />
-                <Container />
-                <Container />
-                <Container />
-                <Container />
+        <>
+            <div className="content-box">
+                <Filter />
+                <Timers />
+                <div className="questions">
+                    <Container />
+                    <Container />
+                    <Container />
+                    <Container />
+                    <Container />
+                </div>
             </div>
-            {/* <Timers /> */}
-        </div>
+
+        </>
+
     )
 }
 export default Content;
